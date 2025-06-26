f106c006e94db13433890837aa46fdbeb3e4a796
MAJETI LASYA <lasya.majeti@gmail.com>
2025-06-26 20:27:49 +0530

    Add files via upload

---
+++ main (14).c
@@ -0,0 +1,17 @@
+//INCREMENTS 
+#include <stdio.h>
+int main() {
+    int a = 5;
+    int b = a++ + ++a;  
+int c= --a + -a;
+    printf("a = %d\n", a);  // Final value of a
+    printf("b = %d\n", b);  // Result of expression
+    printf("c = %d\n", c);  
+    return 0;
+}
+
+
+
+
+
+
